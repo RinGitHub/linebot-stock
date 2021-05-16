c2dd7500b61b7d0bc89baadf049a432599f73c1c
kellylintw <ttt8314@gmail.com>
2021-05-16 18:00:42 +0800

    'test3'

--- app_run.py
+++ app_run.py
@@ -6,7 +6,7 @@
 
 import configparser
 
-from flask import Flask
+from flask import Flask, render_template
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -28,8 +28,12 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def index():
+    return render_template('index.html', data=" Deploying a Flask App To Heroku");
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run(debug=True)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
