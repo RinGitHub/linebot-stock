33786ee11d32279efeae2c25cac138af3f648fa3
kellylintw <ttt8314@gmail.com>
2021-05-16 18:10:38 +0800

    'test4'

--- app_run.py
+++ app_run.py
@@ -4,7 +4,7 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-import configparser
+'''import configparser
 
 from flask import Flask, render_template
 from linebot import (
@@ -15,12 +15,12 @@ app = Flask(__name__)
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-'''line_bot_api = LineBotApi(config['line_bot']['Channel_Access_Token'])
+line_bot_api = LineBotApi(config['line_bot']['Channel_Access_Token'])
 handler = WebhookHandler(config['line_bot']['Channel_Secret'])
 client_id = config['imgur_api']['Client_ID']
 client_secret = config['imgur_api']['Client_Secret']
 album_id = config['imgur_api']['Album_ID']
-API_Get_Image = config['other_api']['API_Get_Image']'''
+API_Get_Image = config['other_api']['API_Get_Image']
 
 
 def print_hi(name):
@@ -29,11 +29,12 @@ def print_hi(name):
 
 
 def index():
-    return render_template('index.html', data=" Deploying a Flask App To Heroku");
+    return render_template('index.html', data=" Deploying a Flask App To Heroku");'''
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    print("hello")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
