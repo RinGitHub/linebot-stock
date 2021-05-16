30bc1e12d0e447ee9ed1603bfe2dde28153cd7aa
kellylintw <ttt8314@gmail.com>
2021-05-16 17:09:00 +0800

    the first upload

--- app_run.py
+++ app_run.py
@@ -4,6 +4,25 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+import configparser
+
+from flask import Flask
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+
+app = Flask(__name__)
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+line_bot_api = LineBotApi(config['line_bot']['Channel_Access_Token'])
+handler = WebhookHandler(config['line_bot']['Channel_Secret'])
+client_id = config['imgur_api']['Client_ID']
+client_secret = config['imgur_api']['Client_Secret']
+album_id = config['imgur_api']['Album_ID']
+API_Get_Image = config['other_api']['API_Get_Image']
+
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
