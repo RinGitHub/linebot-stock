2a23a5313dd08d4f2f5ddf2c332f1cfdc4546662
kellylintw <ttt8314@gmail.com>
2021-05-16 19:03:19 +0800

    'test8'

--- app_run.py
+++ app_run.py
@@ -1,8 +1,4 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import configparser
 
 from flask import Flask, request, abort
 
@@ -12,43 +8,45 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+from linebot.models import *
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+line_bot_api = LineBotApi('kK3nS0J1mu0oDHuFs+W+CFjYLYhDmKOxK2tIaE4YdN7lw4lzhhEEuSqNUvjXimGjc+QW20XzcU+Xy8OID2CLN1AmpnkF6+wIgTN2DokyzpodhyYVMgAK/i2BGvxH+u+iS/hWBqeLIbqBGLzseGdY0QdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('77fdb87ee576fcbee74ebebb7c4d416b'
+
+                         # 監聽所有來自 /callback 的 Post Request
+                         @ app.route("/callback", methods=['POST'])
 
 
-@app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
-
     return 'OK'
+
+    # 處理訊息
 
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token, message)
 
+
+import os
 
 if __name__ == "__main__":
-    app.run()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+)
