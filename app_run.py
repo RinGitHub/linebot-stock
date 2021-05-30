79cbb60638eeadce5b43b5a8d963deca9fa9c81f
kellylintw <ttt8314@gmail.com>
2021-05-31 00:58:01 +0800

    0531_fund_test2

--- app_run.py
+++ app_run.py
@@ -113,12 +113,12 @@ def callback():
                 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
                 chrome_options.add_argument("--disable-dev-shm-usage")
                 chrome_options.add_argument("--no-sandbox")
-                '''driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),
+                driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),
                                           chrome_options=chrome_options)
-                driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(),
-                                          chrome_options=chrome_options)'''
+                '''driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(),
+                                          chrome_options=chrome_options)
                 driver = webdriver.Chrome(executable_path="chromedriver.exe",
-                                          options=chrome_options)
+                                          options=chrome_options)'''
                 driver.maximize_window()
                 driver.get('https://tw.tradingview.com/symbols/TWSE-' + str(text))
                 time.sleep(2)
