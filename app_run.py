0f6fabe67fce0ebf9b279e1c2e7ffd3034bd7800
kellylintw <ttt8314@gmail.com>
2021-06-02 01:27:22 +0800

    0601 fund_6

--- app_run.py
+++ app_run.py
@@ -103,7 +103,7 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    global fn
+    fn = ""
 
     # handle webhook body
     try:
