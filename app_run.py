b159834af93b5f1ffb7eb2b6d69096574db6951a
kellylintw <ttt8314@gmail.com>
2021-06-12 22:59:19 +0800

    0612-7

--- app_run.py
+++ app_run.py
@@ -333,7 +333,7 @@ def callback():
                 content = d_success(text)
                 send_text_message(event, content)
             # 輸入無效指令的回應
-            else:
+            elif text != "幫助":
                 content = "請輸入有效指令或輸入\"幫助\"來查看所有功能。"
                 send_text_message(event, content)
 
